a1156b5f844ecfd66a4f14ef951645816968f694
Oli Dagenais <olivida@microsoft.com>
2015-09-30 14:54:59 -0400

    Create project using maven-archetype-quickstart
    Command line:
    mvn archetype:generate -DarchetypeGroupId=org.apache.maven.archetypes
    -DarchetypeArtifactId=maven-archetype-quickstart
    
    ...values used:
    groupId: com.microsoft.alm
    artifactId: oauth2-useragent
    version: 0.1.0-SNAPSHOT
    package: com.microsoft.alm.oauth2.useragent

---
+++ src/main/java/com/microsoft/alm/oauth2/useragent/App.java
@@ -0,0 +1,13 @@
+package com.microsoft.alm.oauth2.useragent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
