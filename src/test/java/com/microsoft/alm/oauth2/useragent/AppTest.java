34c51b604b823e56c8f0047c125f60755ce31b9c
Oli Dagenais <olivida@microsoft.com>
2015-09-30 15:38:03 -0400

    Upgrade to JUnit 4.11.

--- src/test/java/com/microsoft/alm/oauth2/useragent/AppTest.java
+++ src/test/java/com/microsoft/alm/oauth2/useragent/AppTest.java
@@ -3,39 +3,20 @@
 
 package com.microsoft.alm.oauth2.useragent;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Assert;
+import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
     /**
      * Rigourous Test :-)
      */
+    @Test
     public void testApp()
     {
-        assertTrue( true );
+        Assert.assertTrue(true);
     }
 }
