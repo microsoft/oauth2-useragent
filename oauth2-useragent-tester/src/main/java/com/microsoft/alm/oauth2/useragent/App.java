82d9c68490525559fd2fd20aa3738fbc580956c4
Oli Dagenais <olivida@microsoft.com>
2015-10-05 11:50:43 -0400

    Tester depends on final artifact & demos it.
    Tested with JDK 1.8.0_51 and JDK 1.6.0_20 on Windows.

--- oauth2-useragent-tester/src/main/java/com/microsoft/alm/oauth2/useragent/App.java
+++ oauth2-useragent-tester/src/main/java/com/microsoft/alm/oauth2/useragent/App.java
@@ -3,14 +3,24 @@
 
 package com.microsoft.alm.oauth2.useragent;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+
 /**
- * Hello world!
- *
+ * Very simple program demonstrating the use of the oauth2-useragent library.
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(final String[] args) throws AuthorizationException, URISyntaxException {
+
+        final URI authorizationEndpoint = new URI(args[0]);
+        final URI redirectUri = new URI(args[1]);
+
+        final UserAgentImpl userAgent = new UserAgentImpl();
+
+        final AuthorizationResponse authorizationResponse = userAgent.requestAuthorizationCode(authorizationEndpoint, redirectUri);
+
+        System.out.print("Authorization Code: ");
+        System.out.println(authorizationResponse.getCode());
     }
 }
