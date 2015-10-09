bab6310798e075216d846d7cf35442044fdde248
Oli Dagenais <olivida@microsoft.com>
2015-10-09 17:27:33 -0400

    Add "simple" JavaFx test

---
+++ oauth2-useragent-javafx/src/test/java/com/microsoft/alm/oauth2/useragent/JavaFxTest.java
@@ -0,0 +1,74 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.oauth2.useragent;
+
+import com.sun.javafx.application.LauncherImpl;
+import javafx.application.Platform;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class JavaFxTest {
+
+    private final AtomicReference<Exception> expectedExceptionRef = new AtomicReference<>();
+    private final AtomicReference<Exception> actualExceptionRef = new AtomicReference<>();
+
+    @Before public void before() {
+        JavaFx.RUNNABLE_FACTORY_OVERRIDE = null;
+        expectedExceptionRef.set(null);
+        actualExceptionRef.set(null);
+    }
+
+    @After public void after() {
+        JavaFx.RUNNABLE_FACTORY_OVERRIDE = null;
+        final Exception actualException = this.actualExceptionRef.get();
+        //noinspection ThrowableResultOfMethodCallIgnored
+        final Exception expectedException = this.expectedExceptionRef.get();
+        if (expectedException == null) {
+            if (actualException != null) {
+                throw new Error(actualException);
+            }
+        }
+        else {
+            if (actualException == null) {
+                final String template = "Expected exception '%1$s', got nothing.";
+                final String message = String.format(template, expectedException.getClass().toString());
+                Assert.fail(message);
+            }
+            else {
+                Assert.assertEquals(expectedException.getClass(), actualException.getClass());
+                Assert.assertEquals(expectedException.getMessage(), actualException.getMessage());
+            }
+        }
+    }
+
+    @Category(IntegrationTests.class)
+    @Test public void nullPointerException() {
+        expectedExceptionRef.set(new NullPointerException());
+        final String[] args = {UserAgentImpl.REQUEST_AUTHORIZATION_CODE};
+        JavaFx.RUNNABLE_FACTORY_OVERRIDE = new RunnableFactory() {
+            public Runnable create(final JavaFx javaFx) {
+                return new Runnable() {
+                    public void run() {
+                        try {
+                            javaFx.requestAuthorizationCode(null, null);
+                        }
+                        catch (final Exception e) {
+                            actualExceptionRef.set(e);
+                        }
+                        finally {
+                            Platform.exit();
+                        }
+                    }
+                };
+            }
+        };
+
+        LauncherImpl.launchApplication(JavaFx.class, args);
+    }
+}
