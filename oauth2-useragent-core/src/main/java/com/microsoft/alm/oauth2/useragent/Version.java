c6046ceee3c80b368a5c966482f1637c796d5ea7
leantk <leantk@microsoft.com>
2015-11-05 14:10:06 -0500

    Updated Version to parse for JDK version or generi version

--- oauth2-useragent-core/src/main/java/com/microsoft/alm/oauth2/useragent/Version.java
+++ oauth2-useragent-core/src/main/java/com/microsoft/alm/oauth2/useragent/Version.java
@@ -6,46 +6,103 @@ package com.microsoft.alm.oauth2.useragent;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Version {
+public class Version
+{
     private final static Pattern JAVA_RUNTIME_VERSION =
-            Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)_(\\d+)-(?:.*-)b(\\d+).*");
+            Pattern.compile("(\\d+)\\.(\\d+)\\.(\\d+)_(\\d+)-(?:.*-)?b(\\d+).*");
+    private final static Pattern GENERIC_VERSION =
+            Pattern.compile(".*(\\d+)\\.(\\d+)\\.(\\d+).*");
     private final int major;
     private final int minor;
     private final int patch;
     private final int update;
     private final int build;
 
-    public Version(final String javaRuntimeVersion) {
-        final Matcher matcher = JAVA_RUNTIME_VERSION.matcher(javaRuntimeVersion);
-        if (!matcher.matches()) {
-            final String template = "Unrecognized version string '%1$s'.";
-            final String message = String.format(template, javaRuntimeVersion);
-            throw new IllegalArgumentException(message);
-        }
-        major = Integer.parseInt(matcher.group(1));
-        minor = Integer.parseInt(matcher.group(2));
-        patch = Integer.parseInt(matcher.group(3));
-        update = Integer.parseInt(matcher.group(4));
-        build = Integer.parseInt(matcher.group(5));
+    private Version(int major, int minor, int patch, int update, int build)
+    {
+        this.major = major;
+        this.minor = minor;
+        this.patch = patch;
+        this.update = update;
+        this.build = build;
     }
 
-    public int getMajor() {
+    /**
+     * Parses a Java Runtime version property string to find the details of the JDK version
+     *
+     * @param javaRuntimeVersion JDK version string
+     * @return Version object containing JDK version details
+     */
+    public static Version parseJavaRuntimeVersion(final String javaRuntimeVersion)
+    {
+        Matcher matcher = getMatches(JAVA_RUNTIME_VERSION, javaRuntimeVersion);
+        int major = Integer.parseInt(matcher.group(1));
+        int minor = Integer.parseInt(matcher.group(2));
+        int patch = Integer.parseInt(matcher.group(3));
+        int update = Integer.parseInt(matcher.group(4));
+        int build = Integer.parseInt(matcher.group(5));
+
+        return new Version(major, minor, patch, update, build);
+    }
+
+    /**
+     * Parses a generic string that contains a version number in the format major.minor.patch
+     *
+     * @param version String containing the version number
+     * @return Version object with version details
+     */
+    public static Version parseVersion(final String version)
+    {
+        Matcher matcher = getMatches(GENERIC_VERSION, version);
+        int major = Integer.parseInt(matcher.group(1));
+        int minor = Integer.parseInt(matcher.group(2));
+        int patch = Integer.parseInt(matcher.group(3));
+
+        return new Version(major, minor, patch, 0, 0);
+    }
+
+    /**
+     * Given a pattern and a string, a Matcher is returned if any matches are found else an exception is thrown
+     *
+     * @param pattern Version pattern to match against
+     * @param version String containing the version
+     * @return Matcher with matches to the version pattern
+     */
+    private static Matcher getMatches(final Pattern pattern, final String version)
+    {
+        final Matcher matcher = pattern.matcher(version);
+        if (!matcher.matches())
+        {
+            final String template = "Unrecognized version string '%1$s'.";
+            final String message = String.format(template, version);
+            throw new IllegalArgumentException(message);
+        }
+
+        return matcher;
+    }
+
+    public int getMajor()
+    {
         return major;
     }
 
-    public int getMinor() {
+    public int getMinor()
+    {
         return minor;
     }
 
-    public int getPatch() {
+    public int getPatch()
+    {
         return patch;
     }
 
-    public int getUpdate() {
+    public int getUpdate()
+    {
         return update;
     }
 
-    public int getBuild() {
+    public int getBuild()
+    {
         return build;
     }
 
