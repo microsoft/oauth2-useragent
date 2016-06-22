3c99a94e0b300f8919e3bd28a0b3f73dff534731
Oli Dagenais <olivida@microsoft.com>
2016-06-22 14:29:14 -0400

    Parameterize the provider name for App to use
    This will facilitate re-use for testing with multiple providers.

--- oauth2-useragent-tester/src/main/java/com/microsoft/alm/oauth2/useragent/App.java
+++ oauth2-useragent-tester/src/main/java/com/microsoft/alm/oauth2/useragent/App.java
@@ -18,8 +18,18 @@ public class App
 
         final URI authorizationEndpoint = new URI(args[0]);
         final URI redirectUri = new URI(args[1]);
+        final String providerName = args.length >= 3 ? args[2] : null;
 
         final UserAgent userAgent = new UserAgentImpl();
+        if (providerName != null) {
+            final ProviderScanner providerScanner = (ProviderScanner) userAgent;
+            final Provider provider = providerScanner.findCompatibleProvider(providerName);
+            if (provider == null) {
+                final String template = "The '%s' provider is not available!";
+                final String message = String.format(template, providerName);
+                throw new UnsupportedOperationException(message);
+            }
+        }
 
         final AuthorizationResponse authorizationResponse = userAgent.requestAuthorizationCode(authorizationEndpoint, redirectUri);
 
