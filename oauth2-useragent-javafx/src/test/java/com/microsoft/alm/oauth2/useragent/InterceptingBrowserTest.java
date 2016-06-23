53e63afb2fefd1cb8e26c92e36dc9158ab1145c3
Oli Dagenais <olivida@microsoft.com>
2016-06-23 13:49:21 -0400

    Add InterceptingBrowserTest skeleton

---
+++ oauth2-useragent-javafx/src/test/java/com/microsoft/alm/oauth2/useragent/InterceptingBrowserTest.java
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root.
+
+package com.microsoft.alm.oauth2.useragent;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * A class to test {@link InterceptingBrowser}.
+ */
+public class InterceptingBrowserTest {
+
+}
